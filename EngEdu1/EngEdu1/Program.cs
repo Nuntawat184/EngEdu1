b06de265d881a0d12197284b893b4ba9104e7a8a
Nuntawat184 <58030184@kmitl.ac.th>
2017-10-16 15:32:19 +0700

    2nd  year semester 1

---
+++ EngEdu1/EngEdu1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace EngEdu1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Year 255x semester 1, I am an  EngEdu freshman");
+            Console.WriteLine("Year 255x semester 2, I learn to program is C sharp.");
+            Console.WriteLine("Year 2560 semester 1, I have to regrade on some course.");
+        }
+    }
+}
